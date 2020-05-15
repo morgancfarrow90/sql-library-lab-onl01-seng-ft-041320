f7e33d27e568d57b3cf548cc609917c136e09fbc
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-15 13:16:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM SERIES
-  INNER JOIN authors ON series.author_id = author_id 
+  INNER JOIN authors ON series.author_id = author.id 
+  INNER JOIN subgenres ON series.subgenres_id = subgenres.id
   "
 end
 
