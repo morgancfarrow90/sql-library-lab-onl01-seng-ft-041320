8d2a516ac08267ba33ae6babe64f0ba2514757e7
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-15 15:01:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.id) FROM characters
   INNER JOIN character_books ON characters.id = character_books.character_id
   GROUP BY characters.id
-  ORDER BY COUNT(character_books.id), characters.name;"
+  ORDER BY COUNT(character_books.id) DESC, characters.name;"
 end
