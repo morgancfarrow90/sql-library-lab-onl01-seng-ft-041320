4e72442efeda71d241ca83a2876ee9a856b38d04
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-15 13:08:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,6 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species SUM(species) FROM characters
   ORDER BY(SUM species) DESC
   LIMIT 1;"
-  
 end
 
 def select_name_and_series_subgenres_of_authors
