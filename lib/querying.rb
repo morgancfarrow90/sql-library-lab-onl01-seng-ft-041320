f2ed710ad2b8b4b0ff7ca8a77c284a2123855a68
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-15 12:54:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
-  INNER JOIN series ON books.id = series.id 
+  INNER JOIN series ON books.id = series.id
   WHERE series.id = 1 
   ORDER BY books.year;"
 end
